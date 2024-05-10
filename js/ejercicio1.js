7e1d1a32ca48dfa02109c56b6d70cedd3ec2b8df
CarlosIVela <cvela@udesa.edu.ar>
2024-05-10 13:01:01 -0300

    practica 1 hasta cuarto ejercicio

---
+++ js/ejercicio1.js
@@ -0,0 +1,25 @@
+function rectangulo (base,altura){
+    return base * altura;
+}
+console.log(rectangulo(5, 10));
+
+function triangulo (base,altura){
+    return base * altura /2;
+}
+console.log(triangulo(5, 10));
+
+function largoDelArray(array) {
+    return array.length;
+}
+
+const miArray = [1, 2, 3, 4, 5];
+console.log(largoDelArray(miArray)); 
+
+function cantidadDeLetras(palabra) {
+    return palabra.length;
+}
+
+const mipalabra = "Gallardo";
+console.log(cantidadDeLetras(mipalabra)); 
+
+
